f5e3c15f107b03e5e49f1f2f0352b82ccd6e11a1
Zachary Tan <zht@uw.edu>
2021-02-26 13:57:16 -0800

    add weather api capabilities

---
+++ app.js
@@ -0,0 +1,33 @@
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      console.log("No support");
+    }
+  }
+
+  function showPosition(position) {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    console.log(latitude + " " + longitude);
+    const key = "e14f5933d267004c2d3a342dba46828d";
+    const weather = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&appid=" + key;
+
+    fetch(weather)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            var temp = data.main.temp;
+            var description = data.weather[0].description;
+            var city = data.name;
+            var country = data.sys.country;
+            document.getElementById("weather").innerHTML = "Current temperature in " + city + ", " + country +  " is " + temp + " °C with " + description;
+        })
+
+
+    .catch(err => alert("Wrong city name!"))
+  }
+  
+
+
+  getLocation();
